5a9d4df2ad62022a181ada2244ef9947565b873d
sssh <shihar1994@gmail.com>
2023-07-09 07:56:16 +0530

    Template Changed

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
 
